613f684ea20b47fd83fde8d2089b01bfcefb6e90
Luka komp <lukazrnic5@gmail.com>
2019-04-16 22:57:51 +0200

    Footer Component Added

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <p>All results depends on the Deezer API</p>
+        <p>
+          Made by{" "}
+          <a
+            href="http://www.lukazrnic.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Luka Zrnic
+          </a>
+        </p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
